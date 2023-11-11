c469b8eefa8e3b0e4a55c9f911f57599874f5bf5
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-11-11 14:27:49 +0530

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ['../public/contact.html', '../scripts/contact.js', '../styles/contact.css'];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
+
